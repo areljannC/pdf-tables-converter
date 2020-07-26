abc37583bfada9ac40482e68fd9761d790a1bb96
AJ Clemente <areljann.c@gmail.com>
2020-07-26 00:28:24 -0400

    FIX: Put a terrible implementation of async/await + Promises

--- src/js/main.js
+++ src/js/main.js
@@ -16,73 +16,80 @@ let symbol,
   low = ''
 let columnIndex = 0
 
-loadPDF.promise.then((pdf) => {
-  pdf.getPage(1).then((page) => {
-    page.getTextContent().then((content) => {
+const func = async () => {
+  await loadPDF.promise.then(async (pdf) => {
+    const { numPages } = pdf._pdfInfo
+    for (let i = 1; i <= numPages; i++) {
       let stock = {}
-      content.items.forEach((item) => {
-        const str = item.str.trim()
-        console.log(str)
-        if (
-          str !== 'Alpha Trade Details by Symbol' &&
-          str !== 'TSX Listed Symbols' &&
-          str !== 'Symbol' &&
-          str !== 'Stock Name' &&
-          str !== 'Volume' &&
-          str !== 'Value' &&
-          str !== 'Trades' &&
-          str !== 'Close' &&
-          str !== 'Last Sale' &&
-          str !== 'High' &&
-          str !== 'Low'
-        ) {
-          switch (columnIndex) {
-            case 0:
-              stock = { ...stock, symbol: str }
-              columnIndex++
-              break
-            case 1:
-              stock = { ...stock, name: str }
-              columnIndex++
-              break
-            case 2:
-              stock = { ...stock, volume: str }
-              columnIndex++
-              break
-            case 3:
-              stock = { ...stock, value: str }
-              columnIndex++
-              break
-            case 4:
-              stock = { ...stock, trades: str }
-              columnIndex++
-              break
-            case 5:
-              stock = { ...stock, high: str }
-              columnIndex++
-              break
-            case 6:
-              stock = { ...stock, low: str }
-              columnIndex++
-              break
-            case 7:
-              stock = { ...stock, last_sale: str }
-              columnIndex++
-              break
-            case 8:
-              stock = { ...stock, close: str }
-              columnIndex++
-              break
-            default:
-              break
-          }
-          if (columnIndex === 9) {
-            stocks = [...stocks, stock]
-            columnIndex = 0
-          }
-        }
+      await pdf.getPage(i).then(async (page) => {
+        await page.getTextContent().then(async (content) => {
+          await content.items.forEach((item) => {
+            const str = item.str.trim()
+            if (
+              str !== 'Alpha Trade Details by Symbol' &&
+              str !== 'TSX Listed Symbols' &&
+              str !== 'Symbol' &&
+              str !== 'Stock Name' &&
+              str !== 'Volume' &&
+              str !== 'Value' &&
+              str !== 'Trades' &&
+              str !== 'Close' &&
+              str !== 'Last Sale' &&
+              str !== 'High' &&
+              str !== 'Low'
+            ) {
+              switch (columnIndex) {
+                case 0:
+                  stock = { ...stock, symbol: str }
+                  columnIndex++
+                  break
+                case 1:
+                  stock = { ...stock, name: str }
+                  columnIndex++
+                  break
+                case 2:
+                  stock = { ...stock, volume: str }
+                  columnIndex++
+                  break
+                case 3:
+                  stock = { ...stock, value: str }
+                  columnIndex++
+                  break
+                case 4:
+                  stock = { ...stock, trades: str }
+                  columnIndex++
+                  break
+                case 5:
+                  stock = { ...stock, high: str }
+                  columnIndex++
+                  break
+                case 6:
+                  stock = { ...stock, low: str }
+                  columnIndex++
+                  break
+                case 7:
+                  stock = { ...stock, last_sale: str }
+                  columnIndex++
+                  break
+                case 8:
+                  stock = { ...stock, close: str }
+                  columnIndex++
+                  break
+                default:
+                  break
+              }
+              if (columnIndex === 9) {
+                stocks = [...stocks, stock]
+                columnIndex = 0
+              }
+            }
+          })
+        })
       })
-      console.log(stocks)
-    })
+    }
   })
-})
+  console.log(stocks)
+}
+
+func()
+
